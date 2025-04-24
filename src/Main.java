5a596516745fb01c9dd534ea38c00ee9e7b34c2b
anderson <anderson.bl.filho@gmail.com>
2025-04-24 20:14:22 -0300

    Inclusão na main nome e matricula

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Aluno: Anderson Barbosa De Lira Filho.... Matricula: 1352516970");
         System.out.println("Atividade Completa com Sucesso");
     }
 }
